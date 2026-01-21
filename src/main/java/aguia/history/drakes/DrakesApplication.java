6f1aa90994407b06b0cb3603d080b134f77faefe
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-21 17:10:46 -0300

    Feat: add main class

---
+++ src/main/java/aguia/history/drakes/DrakesApplication.java
@@ -0,0 +1,13 @@
+package aguia.history.drakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrakesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrakesApplication.class, args);
+	}
+
+}
