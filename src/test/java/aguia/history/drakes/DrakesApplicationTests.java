02b1cda16f607efb1a369e19e59f342672f51645
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-21 17:12:34 -0300

    Feat: test files

---
+++ src/test/java/aguia/history/drakes/DrakesApplicationTests.java
@@ -0,0 +1,13 @@
+package aguia.history.drakes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrakesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
