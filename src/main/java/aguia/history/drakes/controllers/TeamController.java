786a6abe47ff74ffe7be42924eae132eacf67ef7
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-21 23:14:37 -0300

    Feat: new endpoint to delete a team by id

--- src/main/java/aguia/history/drakes/controllers/TeamController.java
+++ src/main/java/aguia/history/drakes/controllers/TeamController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 import aguia.history.drakes.domain.Team;
 import aguia.history.drakes.dtos.TeamDTO;
 import aguia.history.drakes.services.TeamService;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -28,6 +31,11 @@ public class TeamController {
         //  retorna o time salvo com DTO
         return ResponseEntity.ok(new TeamDTO(savedTeam));
     }
-
-    
+    // deletar time com id
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTeam(@PathVariable Long id) {
+        teamService.deleteTeam(id);
+        // retorna resposta sem conteúdo (padrao de delete com sucesso)
+        return ResponseEntity.noContent().build();
+    }
 }
