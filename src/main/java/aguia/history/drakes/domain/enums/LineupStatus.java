6d380988e2c4b45a373afa1535652a27769ca3e0
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-22 13:07:50 -0300

    Feat: add lineup status

---
+++ src/main/java/aguia/history/drakes/domain/enums/LineupStatus.java
@@ -0,0 +1,7 @@
+package aguia.history.drakes.domain.enums;
+
+public enum LineupStatus {
+    TITULAR,            // começou jogando
+    RESERVA_ENTROU,     // estava no banco e entrou
+    RESERVA_NAO_ENTROU  // estava no banco e não entrou
+}
