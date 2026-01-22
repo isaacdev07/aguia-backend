1b4fafa07e8dae9b3fdf6bb65c9933180c1e60e7
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-21 22:58:19 -0300

    Feat: refactor player controller to use dtos

--- src/main/java/aguia/history/drakes/controllers/PlayerController.java
+++ src/main/java/aguia/history/drakes/controllers/PlayerController.java
@@ -1,12 +1,15 @@
 package aguia.history.drakes.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import aguia.history.drakes.domain.Player;
+import aguia.history.drakes.dtos.PlayerDTO;
 import aguia.history.drakes.services.PlayerService;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,15 +27,26 @@ public class PlayerController {
 
     // adicionar jogador a um time
     @PostMapping("/team/{teamId}")
-    public ResponseEntity<Player> addPlayer(@PathVariable Long teamId, @RequestBody Player player){ 
-        Player newPlayer = playerService.addPlayerToTeam(teamId, player);
-        return ResponseEntity.ok(newPlayer);
+    public ResponseEntity<PlayerDTO> addPlayer(@PathVariable Long teamId, @RequestBody PlayerDTO dto){ 
+        // dto para entidade
+        Player inputPlayer = dto.toEntity();
+        // salva o jogador
+        Player savedPlayer = playerService.addPlayerToTeam(teamId, inputPlayer);
+        // retorna o jogador salvo com DTO
+        return ResponseEntity.ok(new PlayerDTO(savedPlayer));
     }
 
     // listar jogadores por time
     @GetMapping("/team/{teamId}")
-        public ResponseEntity<List<Player>> listPlayersByTeam(@PathVariable Long teamId){
-            return ResponseEntity.ok(playerService.lisPlayersByTeam(teamId));
+        public ResponseEntity<List<PlayerDTO>> listPlayersByTeam(@PathVariable Long teamId){
+        
+        List<Player> list = playerService.lisPlayersByTeam(teamId);
+
+            List<PlayerDTO> dtoList = list.stream()
+                .map(PlayerDTO::new)
+                .collect(Collectors.toList());
+
+                return ResponseEntity.ok(dtoList);
         }
 
  }
