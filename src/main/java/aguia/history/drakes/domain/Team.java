eaf45a41e08443a90e967d8816640b2815a32ae2
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-21 23:13:33 -0300

    Feat: add relation to players and season

--- src/main/java/aguia/history/drakes/domain/Team.java
+++ src/main/java/aguia/history/drakes/domain/Team.java
@@ -1,12 +1,16 @@
 package aguia.history.drakes.domain;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -19,10 +23,14 @@ public class Team {
 
     @Column(nullable = false)
     private String name;  //nome do time
-
     private LocalDate foundationDate; //data de fundação
-
     private String city; //cidade do time
+    // relação com jogadores
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Player> players = new ArrayList<>();
+    // relação com temporadas
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Season> seasons = new ArrayList<>();
 
     // construtor padrão
     public Team() {}
