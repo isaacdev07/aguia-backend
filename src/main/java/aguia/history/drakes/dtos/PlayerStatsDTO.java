f2047a33158747be4bf2ed7574eaa755061c566a
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-23 11:23:44 -0300

    Feat: create PlayerStatsDTO class to hold player statistics

---
+++ src/main/java/aguia/history/drakes/dtos/PlayerStatsDTO.java
@@ -0,0 +1,12 @@
+package aguia.history.drakes.dtos;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PlayerStatsDTO {
+
+    private String playerName;
+    private Long totalGoals;
+    
+}
