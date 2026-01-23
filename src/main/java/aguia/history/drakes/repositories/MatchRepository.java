796b8beb6abcf5bf1362c16f5a2fba3850277aed
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-23 10:46:30 -0300

    Feat: add method to get matches of especific team

--- src/main/java/aguia/history/drakes/repositories/MatchRepository.java
+++ src/main/java/aguia/history/drakes/repositories/MatchRepository.java
@@ -1,9 +1,13 @@
 package aguia.history.drakes.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import aguia.history.drakes.domain.Match;
 
 public interface MatchRepository extends JpaRepository<Match, Long> {
+    // encontrar partidas por ID da equipe da temporada
+    List<Match> findBySeasonTeamId(Long teamId);
     
-}
+}   
