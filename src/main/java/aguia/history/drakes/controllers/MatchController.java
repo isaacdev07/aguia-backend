3e817daf2cab9b3e18e0b61179d69f1231141559
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-23 10:46:55 -0300

    Feat: add methods to retrieve matches by ID and team, and list all matches

--- src/main/java/aguia/history/drakes/controllers/MatchController.java
+++ src/main/java/aguia/history/drakes/controllers/MatchController.java
@@ -1,7 +1,11 @@
 package aguia.history.drakes.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,4 +28,24 @@ public class MatchController {
         return ResponseEntity.ok(savedMatch);
     }
     
+    // listar todas as partidas
+    @GetMapping
+    public ResponseEntity<List<Match>> findAll() {
+        List<Match> matches = matchService.findAllMatches();
+        return ResponseEntity.ok(matches);
+    }
+
+    //listar partida por id
+    @GetMapping("/{id}")
+    public ResponseEntity<Match> findById(@PathVariable Long id) {
+        Match match = matchService.findMatchById(id);
+        return ResponseEntity.ok(match);
+    }
+
+    //listar partidas por id da equipe
+    @GetMapping("/team/{teamId}")
+    public ResponseEntity<List<Match>> findByTeamId(@PathVariable Long teamId)  {
+        List<Match> matches = matchService.findMatchesByTeam(teamId);
+        return ResponseEntity.ok(matches);
+    }
 }
