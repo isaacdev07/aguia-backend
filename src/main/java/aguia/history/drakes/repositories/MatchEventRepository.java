fb8edabdc2f4b10cea08c4665e5cbcf26bc12365
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-23 11:23:24 -0300

    Feat: add method to retrieve top scorers by team

--- src/main/java/aguia/history/drakes/repositories/MatchEventRepository.java
+++ src/main/java/aguia/history/drakes/repositories/MatchEventRepository.java
@@ -1,9 +1,24 @@
 package aguia.history.drakes.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import aguia.history.drakes.domain.MatchEvent;
+import aguia.history.drakes.dtos.PlayerStatsDTO;
 
 public interface MatchEventRepository extends JpaRepository<MatchEvent, Long> {
     
+    @Query("SELECT new aguia.history.drakes.dtos.PlayerStatsDTO(p.name, COUNT(e)) " +
+           "FROM MatchEvent e " +
+           "JOIN e.player p " +
+           "JOIN p.team t " +
+           "WHERE t.id = :teamId " +
+           "AND e.eventType = 'GOL' " +
+           "GROUP BY p.name " +
+           "ORDER BY COUNT(e) DESC")
+    List<PlayerStatsDTO> findTopScorersByTeam(@Param("teamId") Long teamId);
+
 }
