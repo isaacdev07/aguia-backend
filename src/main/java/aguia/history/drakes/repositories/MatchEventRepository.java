bfa50c1343a7c13fd489336dd581d35efc647250
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-24 20:26:00 -0300

    Feat: new method to get total assists by teamId

--- src/main/java/aguia/history/drakes/repositories/MatchEventRepository.java
+++ src/main/java/aguia/history/drakes/repositories/MatchEventRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import aguia.history.drakes.domain.MatchEvent;
+import aguia.history.drakes.dtos.PlayerAssistDTO;
 import aguia.history.drakes.dtos.PlayerStatsDTO;
 
 public interface MatchEventRepository extends JpaRepository<MatchEvent, Long> {
@@ -20,5 +21,15 @@ public interface MatchEventRepository extends JpaRepository<MatchEvent, Long> {
            "GROUP BY p.name " +
            "ORDER BY COUNT(e) DESC")
     List<PlayerStatsDTO> findTopScorersByTeam(@Param("teamId") Long teamId);
+
+    @Query("SELECT new aguia.history.drakes.dtos.PlayerAssistDTO(p.name, COUNT(e)) " +
+           "FROM MatchEvent e " +
+           "JOIN e.player p " +
+           "JOIN p.team t " +
+           "WHERE t.id = :teamId " +
+           "AND e.eventType = 'ASSISTENCIA' " +
+           "GROUP BY p.name " +
+           "ORDER BY COUNT(e) DESC")
+    List<PlayerAssistDTO> findTopAssistsByTeam(@Param("teamId") Long teamId);
 
 }
