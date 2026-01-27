9a6c2a2244cd5bfea2364e96e4b13c8727755e9d
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-26 22:13:35 -0300

    Feat: implement AuthorizationService for user authentication by email

---
+++ src/main/java/aguia/history/drakes/services/AuthorizationService.java
@@ -0,0 +1,21 @@
+package aguia.history.drakes.services;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import aguia.history.drakes.repositories.UserRepository;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // chama username que é o email
+        return repository.findByEmail(username);
+    }
+}
