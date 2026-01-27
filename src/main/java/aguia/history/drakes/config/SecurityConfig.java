fe93d945335acd8de668d4ad0f7740939c21c301
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-26 22:15:04 -0300

    Feat: enhance SecurityConfig to implement detailed security rules and session management

--- src/main/java/aguia/history/drakes/config/SecurityConfig.java
+++ src/main/java/aguia/history/drakes/config/SecurityConfig.java
@@ -1,21 +1,64 @@
 package aguia.history.drakes.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
-    // Desabilita a segurança para simplificar o desenvolvimento inicial
+    @Autowired
+    SecurityFilter securityFilter;
+
+    // adiciona as regras de segurança nas rotas
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    return http
-        .csrf(csrf -> csrf.disable()) 
-        .authorizeHttpRequests(auth -> auth
-            .anyRequest().permitAll() 
-        )
-        .build();
-}
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable()) 
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) 
+                .authorizeHttpRequests(authorize -> authorize
+                        // rotas de autenticação liberadas
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        
+                        // swagger liberado
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+
+                        // get nas partidas somente autenticado
+                        .requestMatchers(HttpMethod.GET, "/matches").authenticated() 
+                        // criar partidas somente autenticado
+                        .requestMatchers(HttpMethod.POST, "/matches/**").authenticated()
+
+                        //ainda vou adicionar regras específicas para outras rotas
+
+                        // todas as outras requisições precisam estar autenticadas
+                        .anyRequest().authenticated()
+                )
+                // Adiciona o nosso filtro ANTES do filtro padrão do Spring
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    // necessário para autenticação
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    // criptografia de senhas
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
