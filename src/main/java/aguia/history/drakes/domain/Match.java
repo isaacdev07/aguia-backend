211c38fe38f5a4198989c8788522d097730d25bd
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-28 22:35:07 -0300

    Feat: refactor Match class to use Lombok for automatic getter and setter generation

--- src/main/java/aguia/history/drakes/domain/Match.java
+++ src/main/java/aguia/history/drakes/domain/Match.java
@@ -16,7 +16,9 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
 @Entity
 @Table(name = "tb_matches")
 public class Match {
@@ -60,97 +62,4 @@ public class Match {
     @JoinColumn(name = "coach_id", nullable = true) // Pode não ter técnico no jogo
     private Coach coach;
 
-
-    public Match() {}
-
-    public Match(Season season, String opponentName, LocalDateTime matchDate, String location,
-            MatchType matchType, Integer penaltiesFor, Integer penaltiesAgainst, Coach coach) {
-        this.season = season;
-        this.opponentName = opponentName;
-        this.matchDate = matchDate;
-        this.location = location;
-        this.matchType = matchType;
-        this.coach = coach;
-        this.penaltiesFor = penaltiesFor;
-        this.penaltiesAgainst = penaltiesAgainst;
-    }
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public List<MatchEvent> getEvents() {
-        return events;
-    }
-    public void setEvents(List<MatchEvent> events) {
-        this.events = events;
-    }
-    public Integer getGoalsFor() {
-        return goalsFor;
-    }
-    public void setGoalsFor(Integer goalsFor) {
-        this.goalsFor = goalsFor;
-    }
-    public Integer getGoalsAgainst() {
-        return goalsAgainst;
-    }
-    public void setGoalsAgainst(Integer goalsAgainst) {
-        this.goalsAgainst = goalsAgainst;
-    }
-    public Integer getPenaltiesFor() {
-        return penaltiesFor;
-    }
-    public void setPenaltiesFor(Integer penaltiesFor) {
-        this.penaltiesFor = penaltiesFor;
-    }
-    public Integer getPenaltiesAgainst() {
-        return penaltiesAgainst;
-    }
-    public void setPenaltiesAgainst(Integer penaltiesAgainst) {
-        this.penaltiesAgainst = penaltiesAgainst;
-    }   
-    public List<MatchLineup> getLineup() {
-        return lineup;
-    }
-    public void setLineup(List<MatchLineup> lineup) {
-        this.lineup = lineup;
-    }
-    public String getLocation() {
-        return location;
-    }
-    public void setLocation(String location) {
-        this.location = location;
-    }
-    public LocalDateTime getMatchDate() {
-        return matchDate;
-    }
-    public void setMatchDate(LocalDateTime matchDate) {
-        this.matchDate = matchDate;
-    }
-    public MatchType getMatchType() {
-        return matchType;
-    }
-    public void setMatchType(MatchType matchType) {
-        this.matchType = matchType;
-    }
-    public String getOpponentName() {
-        return opponentName;
-    }
-    public void setOpponentName(String opponentName) {
-        this.opponentName = opponentName;
-    }
-    public Season getSeason() {
-        return season;
-    }
-    public void setSeason(Season season) {
-        this.season = season;
-    }
-    public Coach getCoach() {
-        return coach;
-    }   
-    public void setCoach(Coach coach) {
-        this.coach = coach;
-    }
 }
