2f90a21c98facb1bbc819030d702bb21fc95e615
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-28 22:33:10 -0300

    Feat: refactor Player class to use Lombok for automatic getter and setter generation

--- src/main/java/aguia/history/drakes/domain/Player.java
+++ src/main/java/aguia/history/drakes/domain/Player.java
@@ -13,7 +13,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
 @Entity 
 @Table(name = "tb_players")
 public class Player {
@@ -36,42 +38,5 @@ public class Player {
     @JsonIgnore // para evitar recursão infinita na serialização
     private Team team;
 
-    // getters e setters
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public Position getPosition() {
-        return position;
-    }
-    public void setPosition(Position position) {
-        this.position = position;
-    }
-    public Integer getShirtNumber() {
-        return shirtNumber;
-    }
-    public void setShirtNumber(Integer shirtNumber) {
-        this.shirtNumber = shirtNumber;
-    }
-    public Boolean getIsActive() {
-        return isActive;
-    }
-    public void setIsActive(Boolean isActive) {
-        this.isActive = isActive;
-    }
-    public Team getTeam() {
-        return team;
-    }
-    public void setTeam(Team team) {
-        this.team = team;
-    }
 
 }
