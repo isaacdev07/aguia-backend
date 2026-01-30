d7bb394e3d71690a6ec0a60c58660eed2ffd9305
isaacdev07 <isaacsantos7953@gmail.com>
2026-01-30 00:03:40 -0300

    Feat: new enpoints to update, delete and get players active by team

--- src/main/java/aguia/history/drakes/controllers/PlayerController.java
+++ src/main/java/aguia/history/drakes/controllers/PlayerController.java
@@ -11,12 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 import aguia.history.drakes.domain.Player;
 import aguia.history.drakes.dtos.PlayerCreateDTO;
 import aguia.history.drakes.dtos.PlayerDTO;
+import aguia.history.drakes.dtos.PlayerUpdateDTO;
 import aguia.history.drakes.services.PlayerService;
 import jakarta.validation.Valid;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -34,6 +37,33 @@ public class PlayerController {
         Player savedPlayer = playerService.createPlayer(dto);
         // retorna o jogacor criado com dto
         return ResponseEntity.ok(new PlayerDTO(savedPlayer));
+    }
+
+    // atualizar jogador
+    @PutMapping("/{id}")
+    public ResponseEntity<PlayerDTO> update(@PathVariable Long id, @RequestBody @Valid PlayerUpdateDTO dto) {
+        // pega o jogador atualizado no service
+        Player updatedPlayer = playerService.updatePlayer(id, dto);
+        // retorna o jogador atualizado com dto
+        return ResponseEntity.ok(new PlayerDTO(updatedPlayer));
+    }
+
+    //deletar jogador (soft delete)
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        playerService.deletePlayer(id);
+        return ResponseEntity.noContent().build(); // Retorna 204 No Content
+    }
+
+    // listar jogadores ativos por time
+    @GetMapping("/team/{teamId}/active")
+    public ResponseEntity<List<PlayerDTO>> getActivePlayers(@PathVariable Long teamId) {
+        // busca jogadores ativos pelo time
+        List<Player> players = playerService.findActivePlayersByTeam(teamId);
+        // monta a lista de dtos
+        List<PlayerDTO> dtos = players.stream().map(PlayerDTO::new).toList();
+        // retorna a lista
+        return ResponseEntity.ok(dtos);
     }
 
     // listar jogadores por time
