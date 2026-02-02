ab2ab2e0d9af45991134dc99ab172e5275e02ded
isaacdev07 <isaacsantos7953@gmail.com>
2026-02-01 22:33:44 -0300

    Feat: update repository to implements methods to get active matches

--- src/main/java/aguia/history/drakes/repositories/MatchRepository.java
+++ src/main/java/aguia/history/drakes/repositories/MatchRepository.java
@@ -1,13 +1,25 @@
 package aguia.history.drakes.repositories;
 
 import java.util.List;
+import java.util.Optional; 
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import aguia.history.drakes.domain.Match;
 
 public interface MatchRepository extends JpaRepository<Match, Long> {
-    // encontrar partidas por ID da equipe da temporada
+
+    // traz todas as partidas de um time específico
     List<Match> findBySeasonTeamId(Long teamId);
     
-}   
+
+    // listar todas as partidas ativas
+    List<Match> findByIsActiveTrue();
+
+    // buscar partida ativa por id
+    Optional<Match> findByIdAndIsActiveTrue(Long id);
+
+    // listar partidas ativas de um time específico
+    List<Match> findBySeasonTeamIdAndIsActiveTrue(Long teamId);
+
+}
