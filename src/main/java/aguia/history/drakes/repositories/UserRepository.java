34717c6fd5a22ad5b0755942e98a3fac0794ca4b
isaacdev07 <isaacsantos7953@gmail.com>
2026-02-05 00:44:55 -0300

    Feat: update method findByEmail to optional

--- src/main/java/aguia/history/drakes/repositories/UserRepository.java
+++ src/main/java/aguia/history/drakes/repositories/UserRepository.java
@@ -1,13 +1,11 @@
 package aguia.history.drakes.repositories;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
-
 import aguia.history.drakes.domain.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    // buscar usuario pelo email
-    UserDetails findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
