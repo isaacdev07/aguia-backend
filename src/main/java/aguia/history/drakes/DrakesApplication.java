fffafc377d56ab339b2f250e9653f67d48c46ba9
isaacdev07 <isaacsantos7953@gmail.com>
2026-02-05 00:45:53 -0300

    Feat: add scan to find new service class

--- src/main/java/aguia/history/drakes/DrakesApplication.java
+++ src/main/java/aguia/history/drakes/DrakesApplication.java
@@ -2,8 +2,10 @@ package aguia.history.drakes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "aguia.history.drakes")
 public class DrakesApplication {
 
 	public static void main(String[] args) {
