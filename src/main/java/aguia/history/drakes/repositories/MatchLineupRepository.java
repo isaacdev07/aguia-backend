172f7146dc856aa28fe5cc9238cd070e90aa4f60
isaacdev07 <isaacsantos7953@gmail.com>
2026-02-09 20:42:57 -0300

    Feat: add method to count player participation by status in MatchLineupRepository

--- src/main/java/aguia/history/drakes/repositories/MatchLineupRepository.java
+++ src/main/java/aguia/history/drakes/repositories/MatchLineupRepository.java
@@ -3,7 +3,10 @@ package aguia.history.drakes.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import aguia.history.drakes.domain.MatchLineup;
+import aguia.history.drakes.domain.enums.LineupStatus;
 
 public interface MatchLineupRepository extends JpaRepository<MatchLineup, Long> {
-    
+    // Contar quantas vezes um jogador participou como titular, reserva ou não participou
+    Long countByPlayerIdAndStatus(Long playerId, LineupStatus status);
+
 }
