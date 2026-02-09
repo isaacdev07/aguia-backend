26a2436e5da1df921f629c5c27d0bad374008909
isaacdev07 <isaacsantos7953@gmail.com>
2026-02-09 20:41:18 -0300

    Feat: now the older PlayerStatsDTO get a new name: TopScorerDTO

---
+++ src/main/java/aguia/history/drakes/dtos/TopScorerDTO.java
@@ -0,0 +1,12 @@
+package aguia.history.drakes.dtos;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TopScorerDTO {
+
+    private String playerName;
+    private Long totalGoals;
+    
+}
