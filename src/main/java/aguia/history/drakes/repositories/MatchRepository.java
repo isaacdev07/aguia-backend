95a63527c7ecb26db77afa3c318c1f09519c9144
isaacdev07 <isaacsantos7953@gmail.com>
2026-02-16 22:12:40 -0300

    Feat: add query to retrieve top clean sheets for a specific team

--- src/main/java/aguia/history/drakes/repositories/MatchRepository.java
+++ src/main/java/aguia/history/drakes/repositories/MatchRepository.java
@@ -4,7 +4,10 @@ import java.util.List;
 import java.util.Optional; 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import aguia.history.drakes.dtos.TopCleanSheetDTO;
 import aguia.history.drakes.domain.Match;
 
 public interface MatchRepository extends JpaRepository<Match, Long> {
@@ -21,5 +24,17 @@ public interface MatchRepository extends JpaRepository<Match, Long> {
 
     // listar partidas ativas de um time específico
     List<Match> findBySeasonTeamIdAndIsActiveTrue(Long teamId);
+
+    @Query("SELECT new aguia.history.drakes.dtos.TopCleanSheetDTO(p.name, COUNT(m)) " +
+       "FROM MatchLineup ml " +
+       "JOIN ml.match m " +
+       "JOIN ml.player p " +
+       "WHERE p.team.id = :teamId " +
+       "AND p.position = 'GOLEIRO' " + 
+       "AND ml.status = 'TITULAR' " +
+       "AND m.goalsAgainst = 0 " +      
+       "GROUP BY p.name " +
+       "ORDER BY COUNT(m) DESC")
+List<TopCleanSheetDTO> findTopCleanSheets(@Param("teamId") Long teamId);
 
 }
